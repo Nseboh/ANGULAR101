d95a4e4e158adaaaa0d153d26e63c83eeef18f43
Nseboh <nsebohsampson@gmail.com>
2024-08-26 12:46:28 +0000

    updated

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { CardComponent } from "./card/card.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FirstComponentComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
